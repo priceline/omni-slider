6500c7edd17fe7727cc4ec39da54ec6ee5bb3e5b
Chris Ng <chrisrcng@gmail.com>
2016-06-12 13:32:27 -0400

    Update 2016-06-12T17:32:25.657Z

--- gulp/test.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import jasmine from 'gulp-jasmine';
-import { testFiles } from './gulp-config';
-
-gulp.task('test', function () {
-  return gulp.src(testFiles)
-    .pipe(jasmine());
-});
